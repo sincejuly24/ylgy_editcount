eaa17b6c06effc8ec0b3f5d07982855839c201e0
Mr.Robot <31414964+1nfty@users.noreply.github.com>
2022-09-18 00:02:47 +0800

    Create ylgy_leaderboard.js

---
+++ ylgy_leaderboard.js
@@ -0,0 +1,12 @@
+ar body = $response.body
+var url = $request.url
+
+if (body) {
+  var obj = JSON.parse($response.body)
+  obj.data.daily_count =
+    '987654321'
+
+  $done({ body: JSON.stringify(obj) })
+} else {
+  $done({})
+}
