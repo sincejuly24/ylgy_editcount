a3d18a5ac0dc4dee4f399140efeebd13d208b85a
coderalfred <63139863+sincejuly24@users.noreply.github.com>
2022-09-19 11:22:36 +0800

    Update ylgy_leaderboard.js

--- ylgy_leaderboard.js
+++ ylgy_leaderboard.js
@@ -4,7 +4,7 @@ var url = $request.url
 if (body) {
   var obj = JSON.parse($response.body)
   obj.data.daily_count =
-    '987654321'
+    '99999999'
 
   $done({ body: JSON.stringify(obj) })
 } else {
